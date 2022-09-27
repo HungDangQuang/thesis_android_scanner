f032b625b659e2445e9e4bb001d66f7effdf7678
HungDangQuang <hungdangkg@gmail.com>
2022-09-27 20:16:42 +0700

    add text input behaviour

--- app/src/main/java/uit/app/document_scanner/ReviewImageActivity.java
+++ app/src/main/java/uit/app/document_scanner/ReviewImageActivity.java
@@ -5,6 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
@@ -18,11 +22,12 @@ import java.io.FileNotFoundException;
 
 import uit.app.document_scanner.openCV.OpenCVUtils;
 
-public class ReviewImageActivity extends AppCompatActivity {
+public class ReviewImageActivity extends AppCompatActivity implements View.OnClickListener {
 
     ImageView reviewImage;
     LinearLayout sourceFrame;
     EditText editText;
+    Button removeTextButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,6 +37,8 @@ public class ReviewImageActivity extends AppCompatActivity {
         sourceFrame = findViewById(R.id.sourceImageView);
         reviewImage = findViewById(R.id.review_image);
         editText = findViewById(R.id.filename);
+        editText.setFocusable(true);
+        removeTextButton = findViewById(R.id.removeTextButton);
 
         sourceFrame.post(new Runnable() {
             @Override
@@ -40,6 +47,7 @@ public class ReviewImageActivity extends AppCompatActivity {
                 Uri uri = intent.getParcelableExtra("croppedImage");
                 File filename = new File(uri.getLastPathSegment());
                 editText.setText(filename.toString());
+
                 try {
                     Bitmap bm = new AppUtils().getBitmap(uri,ReviewImageActivity.this);
 
@@ -54,10 +62,57 @@ public class ReviewImageActivity extends AppCompatActivity {
                 }
             }
         });
+
+        removeTextButton.setOnClickListener(this);
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                if(i1 != 0) {
+//                    removeTextButton.setAlpha(1);
+//                    removeTextButton.setEnabled(true);
+//                }
+//                else {
+//                    removeTextButton.setAlpha(0);
+//                    removeTextButton.setEnabled(false);
+//                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if(editText.getText().length() == 0) {
+                    removeTextButton.setAlpha(0);
+                    removeTextButton.setEnabled(false);
+                }
+                else {
+                    removeTextButton.setAlpha(1);
+                    removeTextButton.setEnabled(true);
+                }
+            }
+        });
+
     }
+
+
 
     @Override
     public void onBackPressed() {
         super.onBackPressed();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.removeTextButton:
+                editText.setText("");
+                view.setEnabled(false);
+                view.setAlpha(0);
+                break;
+        }
     }
 }
