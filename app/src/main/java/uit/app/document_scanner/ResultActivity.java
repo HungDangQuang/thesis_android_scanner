1fcc8fbe82ae7de95abd8e81887aac64eb0cce37
HungDangQuang <hungdangkg@gmail.com>
2022-12-13 17:10:42 +0700

    basic set up for activity result

--- app/src/main/java/uit/app/document_scanner/ResultActivity.java
+++ app/src/main/java/uit/app/document_scanner/ResultActivity.java
@@ -2,19 +2,33 @@ package uit.app.document_scanner;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
 public class ResultActivity extends OptionalActivity {
 
+    private EditText editableID;
+    private EditText editableName;
+    private EditText editableDOB;
+    private EditText editableHometown;
+    private EditText editableAddress;
+
+
     @Override
     protected void init() {
         super.init();
+        editableID = findViewById(R.id.editableID);
+        editableName = findViewById(R.id.editableName);
+        editableDOB = findViewById(R.id.editableDOB);
+        editableHometown = findViewById(R.id.editableHometown);
+        editableAddress = findViewById(R.id.editableAddress);
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
     }
 
     @Override
