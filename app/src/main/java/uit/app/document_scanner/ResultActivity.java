2bcff5604debbaf49b13435bb0a59f2526bc8535
HungDangQuang <hungdangkg@gmail.com>
2022-12-14 11:59:54 +0700

    change ui color

--- app/src/main/java/uit/app/document_scanner/ResultActivity.java
+++ app/src/main/java/uit/app/document_scanner/ResultActivity.java
@@ -1,19 +1,33 @@
 package uit.app.document_scanner;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ResultActivity extends OptionalActivity {
+
+    private static String TAG = ResultActivity.class.getSimpleName();
+    final Calendar myCalendar= Calendar.getInstance();
 
     private EditText editableID;
     private EditText editableName;
     private EditText editableDOB;
     private EditText editableHometown;
     private EditText editableAddress;
-
 
     @Override
     protected void init() {
@@ -23,6 +37,25 @@ public class ResultActivity extends OptionalActivity {
         editableDOB = findViewById(R.id.editableDOB);
         editableHometown = findViewById(R.id.editableHometown);
         editableAddress = findViewById(R.id.editableAddress);
+        editableDOB.setFocusable(false);
+
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+        };
+
+        editableDOB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(ResultActivity.this,date,myCalendar.get(Calendar.DAY_OF_MONTH),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.YEAR));
+                datePickerDialog.show();
+            }
+        });
     }
 
     @Override
@@ -43,4 +76,11 @@ public class ResultActivity extends OptionalActivity {
         return super.onCreateOptionsMenu(menu);
 
     }
+
+    private void updateLabel(){
+        String myFormat="dd-mm-yyyy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        editableDOB.setText(dateFormat.format(myCalendar.getTime()));
+    }
+
 }
