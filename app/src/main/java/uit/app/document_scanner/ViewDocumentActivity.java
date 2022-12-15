d738c90032931a5242b762a6565ee2b8deecc2dc
HungDangQuang <hungdangkg@gmail.com>
2022-12-15 12:02:09 +0700

    basic set up for view document activity

--- app/src/main/java/uit/app/document_scanner/ViewDocumentActivity.java
+++ app/src/main/java/uit/app/document_scanner/ViewDocumentActivity.java
@@ -1,5 +1,28 @@
 package uit.app.document_scanner;
 
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+
 public class ViewDocumentActivity extends OptionalActivity {
 
+    private ImageView imageView;
+
+    @Override
+    protected void init() {
+        super.init();
+        imageView = findViewById(R.id.resultImage);
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return R.layout.activity_view_document;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
 }
