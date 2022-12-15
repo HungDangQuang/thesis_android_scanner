57ceebb4c4ab576f8cb42f5c205f68ca3145fd90
HungDangQuang <hungdangkg@gmail.com>
2022-12-15 17:14:22 +0700

    add function for buttons

--- app/src/main/java/uit/app/document_scanner/ViewDocumentActivity.java
+++ app/src/main/java/uit/app/document_scanner/ViewDocumentActivity.java
@@ -2,7 +2,10 @@ package uit.app.document_scanner;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -11,14 +14,26 @@ import com.squareup.picasso.Picasso;
 
 import java.io.File;
 
-public class ViewDocumentActivity extends OptionalActivity {
+public class ViewDocumentActivity extends OptionalActivity implements View.OnClickListener {
 
+    private static String TAG = ViewDocumentActivity.class.getSimpleName();
     private ImageView imageView;
+    private Button addNewDocumentButton;
+    private Button ocrButton;
+    private Button shareDocumentButton;
 
     @Override
     protected void init() {
         super.init();
         imageView = findViewById(R.id.resultImage);
+        addNewDocumentButton = findViewById(R.id.addNewDocumentButton);
+        ocrButton = findViewById(R.id.ocrButton);
+        shareDocumentButton = findViewById(R.id.shareButton);
+
+        // set on click listener
+        addNewDocumentButton.setOnClickListener(this);
+        ocrButton.setOnClickListener(this);
+        shareDocumentButton.setOnClickListener(this);
 
         // show back button on action bar
         assert getSupportActionBar() != null;
@@ -51,5 +66,29 @@ public class ViewDocumentActivity extends OptionalActivity {
     public boolean onSupportNavigateUp() {
         finish();
         return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.addNewDocumentButton:
+                Intent addDocumentIntent = new Intent(ViewDocumentActivity.this,CameraActivity.class);
+                startActivity(addDocumentIntent);
+                break;
+
+            case R.id.ocrButton:
+                // for testing
+                Intent ocrIntent = new Intent(ViewDocumentActivity.this, ResultActivity.class);
+                startActivity(ocrIntent);
+                break;
+
+            case R.id.shareButton:
+                Log.d(TAG, "onClick: display share pop up");
+                break;
+
+            default:
+                break;
+        }
     }
 }
