cb8ed4a133c6e2957f7d19289fa8d4806aa14c92
HungDangQuang <hungdangkg@gmail.com>
2022-12-15 12:37:42 +0700

    add back button on action bar

--- app/src/main/java/uit/app/document_scanner/ViewDocumentActivity.java
+++ app/src/main/java/uit/app/document_scanner/ViewDocumentActivity.java
@@ -1,6 +1,7 @@
 package uit.app.document_scanner;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +14,10 @@ public class ViewDocumentActivity extends OptionalActivity {
     protected void init() {
         super.init();
         imageView = findViewById(R.id.resultImage);
+
+        // show back button on action bar
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -24,5 +29,17 @@ public class ViewDocumentActivity extends OptionalActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         init();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_view,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
     }
 }
